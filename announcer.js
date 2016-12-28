1af9732a46f068353c2b5be866a94f3007e807d2
hollomancer <chollomon@gmail.com>
2016-12-27 22:02:21 -0500

    First working version

--- announcer.js
+++ announcer.js
@@ -1,16 +1,46 @@
 require('dotenv').config()
-var hook_url = "https://hooks.slack.com/services/" + process.env.SLACK_TOKEN;
 
-var Slack = require('node-slack');
-var slack = new Slack(hook_url);
+const Airtable = require('airtable');
+const hook_url = "https://hooks.slack.com/services/" + process.env.SLACK_TOKEN;
+const CronJob = require('cron').CronJob;
+const Slack = require('node-slack');
+const slack = new Slack(hook_url);
+const base = new Airtable({
+    apiKey: process.env.AIRTABLE_API_KEY
+}).base(process.env.AIRTABLE_BASE);
 
-slack.send({
-    text: 'Our scholarship licenses for Treehouse web development and design, from https://www.teamtreehouse.com/, are still available! You can request a license at http://op.co.de/scholarship-license.',
-    channel: '#test',
-    username: 'Scholarship Announcement'
+base('Slack Announcer').select({
+    view: "Announcer Filter"
+}).eachPage(function page(records, fetchNextPage) {
+
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function (record) {
+
+        var sec = record.get('Second');
+        var min = record.get('Minute');
+        var hor = record.get('Hour');
+        var dom = record.get('Day of Month');
+        var mon = record.get('Month');
+        var dow = record.get('Day of Week');
+
+        new CronJob(sec + ' ' + min + ' ' + hor + ' ' + dom + ' ' + mon + ' ' + dow, function () {
+            slack.send({
+                text: record.get('Text'),
+                channel: record.get('Channels').toString(),
+                username: record.get('Announcer Name')
+            });
+        }, null, true, 'America/Los_Angeles');
+
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(error) {
+    if (error) {
+        console.log(error);
+    }
 });
-
-var CronJob = require('cron').CronJob;
-new CronJob('* * * * * *', function () {
-    console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
