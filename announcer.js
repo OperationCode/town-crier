8c332e3cee97b348d1380a35cbabb14213ee8029
Sean McBride <spmcbride1201@gmail.com>
2017-03-03 18:47:23 -0500

    Periodic refresh (#6)
    * Change jobs to run outside of table refresh and add logging

--- announcer.js
+++ announcer.js
@@ -7,47 +7,72 @@ const base = new Airtable({
     apiKey: process.env.AIRTABLE_API_KEY
 }).base(process.env.AIRTABLE_BASE);
 
-base('Slack Announcer').select({
-    view: "Announcer Filter"
-}).eachPage(function page(records, fetchNextPage) {
+var airtableCronJobs = [];
 
-    // This function (`page`) will get called for each page of records.
+// Flush and reload the CRON table at 3 minutes and 3 seconds past every hour
+// This is specifically offset from 5, 10, 15 minute intervals to ensure that 
+// a CRON job is not set to fire whe the CRON table is being refreshed
 
-    records.forEach(function (record) {
+const update_cron = '3 3 * * * *';
 
-        var sec = record.get('Second');
-        var min = record.get('Minute');
-        var hor = record.get('Hour');
-        var dom = record.get('Day of Month');
-        var mon = record.get('Month');
-        var dow = record.get('Day of Week');
+new CronJob(update_cron, function () {
+    // Explicitly stop all airtable CRON jobs 
+    airtableCronJobs.forEach(function (job) {
+        job.stop();
+    })
 
-        var airtable_cron = (sec + ' ' + min + ' ' + hor + ' ' + dom + ' ' + mon + ' ' + dow);
+    // Flush all CronJob instances
+    airtableCronJobs = [];
 
-        new CronJob(airtable_cron, function () {
+    // Refresh and start all CronJob instances from airtable
+    base('Slack Announcer').select({
+        view: "Announcer Filter"
+    }).eachPage(function page(records, fetchNextPage) {
 
+        // This function (`page`) will get called for each page of records.
 
-            // See what channels are associated with this entry.
-            record.get('Channels').forEach(function (channel) {
+        records.forEach(function (record) {
 
-                slack.send({
-                    text: record.get('Text'),
-                    channel: channel.toString(),
-                    username: record.get('Announcer Name')
-                });
+            var name = record.get('Name');
+            var sec = record.get('Second');
+            var min = record.get('Minute');
+            var hor = record.get('Hour');
+            var dom = record.get('Day of Month');
+            var mon = record.get('Month');
+            var dow = record.get('Day of Week');
 
-            })
-        }, null, true, 'America/Los_Angeles');
+            if (min > 1 && min < 5) {
+                min = 5;
+                console.log(`job ${name} was modified to run outside of the CRON table refresh window`);
+            }
 
+            var airtable_cron = (sec + ' ' + min + ' ' + hor + ' ' + dom + ' ' + mon + ' ' + dow);
+
+            airtableCronJobs.push(new CronJob(airtable_cron, function () {
+                console.log(`Running job ${name}`);
+
+                // See what channels are associated with this entry.
+                record.get('Channels').forEach(function (channel) {
+
+                    slack.send({
+                        text: record.get('Text'),
+                        channel: channel.toString(),
+                        username: record.get('Announcer Name')
+                    });
+
+                })
+            }, null, true, 'America/Los_Angeles'));
+
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(error) {
+        if (error) {
+            console.log(error);
+        }
     });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(error) {
-    if (error) {
-        console.log(error);
-    }
 });
